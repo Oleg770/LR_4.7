6d7772c902aa6336ad5d7fa3a05f4819cfca2111
Oleg770 <Oleh.Kalka.ITIS.2020@lpnu.ua>
2020-11-02 18:25:49 +0300

    LR_4.7

---
+++ Source.cpp
@@ -0,0 +1,52 @@
+// Лабораторна робота № 4.7
+// < Калька Олег >
+// Варіант 04
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double xp, xk, x, dx, eps, a, R = 0, S = 0;
+	int n = 0;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+
+	    cout << "-------------------------------------------------" << endl;
+		cout << "|" << setw(5)  << "x"      << "     |"
+			        << setw(10) <<     "1.*n(x+1)  |"
+			        << setw(7) << "S"      << "      |"
+			        << setw(5)  << "n"      << "   |"
+			        << endl;
+		cout << "-------------------------------------------------" << endl;
+		x = xp;
+		while (x <= xk)
+		{
+			n = 0;
+			a = 1;
+            S = a;
+			do {
+				n++;
+				R = -x * (n / (n + 1));
+			    a *= R;
+			    S += a;
+			} while (abs(a) >= eps);
+
+			cout << "|" << setw(7) << setprecision(2) << x << "    |"
+				        << setw(10) << setprecision(5) << 1.*n*(x + 1) << "    |"
+				        << setw(10) << setprecision(5) << S << "    |"
+				        << setw(5) << n << "    |"
+				; x += dx;
+		}
+		cout << "-------------------------------------------------" << endl;
+		 
+		 return 0;
+}
